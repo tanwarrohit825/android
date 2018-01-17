0901436629a4a68d2d8e4e56746859a95869d48f
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-01-17 22:01:20 +0530

    about small thing (android)
    In this we learn android very basic thing

---
+++ imp.sh
@@ -0,0 +1,61 @@
+=======create a new XML file======
+
+in android view -> layout -> right click ->new -> Layout resource file
+
+----add java class -----
+
+in android view -> java -> <your package name> ->right click -> new -> create a new java class
+
+-----use XML ----------
+
+
+View <store valiable> = inflater.inflate(R.layout.<fragement name>,container,false);
+return <store valiable>;
+
+
+
+-----------add referance ot the valiable ----------
+
+#defiine the valiable
+private staic <valiable type> <valiable name>;
+
+	#assine the valiable
+	<valiable name> = (<valiable type>) view.findViewByid(R.id.<valiable name>);
+
+	#assine in the button
+	final Button <button name> =(Button) view.findViewByid(R.id.<button name>);
+
+
+------------ animation and transitions ----------------
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+
+	#referance the layout
+	ViewGroup <layout name>;
+
+	#in onCreate
+	<layout name> = (ViewGroup) findViewByid(R.id.<layout name>);
+
+	<layout name>.setOnTouchListener(){
+		public boolean onTouch(View v,MotionEvent event){
+			moveButton();
+			return true;
+
+		}
+	}
+
+	public void moveButton(){
+		View <button name> = findViewByid(R.id.<button id>);
+
+		#change the postion of the button
+		RelativeLayout.LayoutParams postionRule = new RelativeLayout.LayoutParams(
+			RelativeLayout.LayoutParams.WRAP_CONNTENT,RelativeLayout.LayoutParams.WRA _CONNTENT);
+}
+
+--------------switch btw to activity-------
+import android.content.Intent;
+import android.
