01a6aeeb627f34cd1d44bd46ee01752bff2c29a9
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-01-25 15:29:40 +0530

    Bound service

--- type of service.sh
+++ type of service.sh
@@ -84,6 +84,67 @@ startSevrice(Intent variable);
 
 
 
+------------Bound Service-----------------
+#in MyService
+import android.os.Binder;
+import java.text.SimpleDataFormate;
+import java.util.Date;
+import java.util.Locale;
+//create a object
+	private final IBinder <IBinder object name> = new  <new java class name>();
+
+
+#in MyService
+public class <new java class name> extend Binder{
+	MyService getService(){
+		return MyService.this;
+	}
+}
+
+#in onBind
+return <IBinder object> ;
+
+#in MyService
+public String getCurrentTime(){
+	SimpleDataFormate df = new SimpleDataFormate("HH:mm:ss",Local.us);
+	return (df.format(new Date()));
+}
+
+++++++ in main class
+
+import android.view.View;
+import android.widget.TextView;
+import android.os.IBinder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import <package name(MyService class)>.MyService.<binder name>;
+
+#in main class
+//MyService object
+	MyService <my Service object>;
+	boolean is = false;
+
+private ServiceConnection <ServiceConnection name> = new ServiceConnection(){
+	#in onServiceConnect
+	<binder name> binder = (MyLocalBinder) service;
+	<service name> = binder.getService();
+	isBound = true;
+
+	#in onServiceDisconnect
+	isBound = false;
+
+#in onCreate
+Intent i = new Intent(this,<service class name>.class);
+binderService(i,<ServiceConnection name>,Context.BIND_AUTO_CREATE);
+}
+
+#in main class
+public void <onClick methode name> (View view){
+	String <time class> = <service name>.getCurrentTime();
+	TextView <Text name> = (TextView) findViewById(R.id.<text id>);
+	<text name>.SetText(<time class>);
+}
 
 
 
